9b36e2b53043f2890b42639386e23ebb407a8013
s4f4y4t <safayat.me@gmail.com>
2025-10-08 09:46:53 +0600

    Hotfix: mail content

--- resources/views/emails/bills/created.blade.php
+++ resources/views/emails/bills/created.blade.php
@@ -5,7 +5,7 @@ A new bill has been created for you.
 - **Type:** {{ $bill->category->name }}
 - **Month:** {{ \Carbon\Carbon::createFromFormat('Y-m', $bill->month)->format('F Y') }}
 - **Total Amount:** {{ number_format($bill->total_amount, 2) }}
-- **Status:** {{ ucfirst($bill->status) }}
+- **Status:** {{ ucfirst($bill->payment_status) }}
 
 Thanks,<br>
 {{ config('app.name') }}
--- resources/views/emails/bills/paid.blade.php
+++ resources/views/emails/bills/paid.blade.php
@@ -5,7 +5,7 @@ Your bill has been paid.
 - **Type:** {{ $bill->category->name }}
 - **Month:** {{ \Carbon\Carbon::createFromFormat('Y-m', $bill->month)->format('F Y') }}
 - **Total Amount:** {{ number_format($bill->total_amount, 2) }}
-- **Status:** {{ ucfirst($bill->status) }}
+- **Status:** {{ ucfirst($bill->payment_status) }}
 
 Thanks,<br>
 {{ config('app.name') }}
